883960369dfe9a965d3b60a276aa1637d65f86fe
jaredgoldman <jaredgoldmandrums@gmail.com>
2021-06-07 15:19:45 -0400

    ignore accounts seed

--- crypto-server/db/seeds/03_accounts.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO
-accounts(account_name, user_id, exchange_id, api_key, api_secret)
-VALUES
-('jg_kraken', 1, 5, 'ZcDMCpbR04YCSqfcCzhM2rDj/NHS2vTgqWIqQlkhnMaFbMHqMTJi4K/z', 'qFo4+9KoUpjtyijPCTV/B/f7ROKA+M7DxbawvekvbGI7CdnpWjAKa//wypem7k6DtMW+1TlJiFeu0zlZMbRPEw==');
